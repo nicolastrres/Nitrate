fd51928980de0e9fa14e9bc44058e370b2d678e6
Nicolas Agustin Torers <nicolastrres@gmail.com>
2016-06-03 08:12:10 -0300

    Migrating from MySQL to postgres

--- tcms/settings/devel.py
+++ tcms/settings/devel.py
@@ -1,21 +1,20 @@
 # Django settings for devel env.
+import os
 
 from common import *
 
 # Debug settings
+from tcms.settings.common import TCMS_ROOT_PATH
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 # Database settings
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'nitrate',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nitrate',
         'USER': 'nitrate',
-        'PASSWORD': 'nitrate',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
     }
 }
 
